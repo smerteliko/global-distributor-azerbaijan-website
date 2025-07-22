744f17a2d1852ac43936981ae8c13f1cf870a9d7
smerteliko <smerteliko@gmail.com>
2025-07-22 23:32:32 +0300

    Changes for github pages

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.VUE_APP_BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
